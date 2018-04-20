80b1c1354c867ad0b469307a46cb2213fd4377af
Wagura Maurice <wagura465@gmail.com>
2018-04-20 10:02:47 +0300

    create server in app.js, listening on port 3000

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("testing 123... mic check");
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var host = "127.0.0.1";
+var port = 3000;
+
+// app.get('/', fun)
+
+app.listen(port, function() {
+	console.log('Server runing on port' + port)
+})
