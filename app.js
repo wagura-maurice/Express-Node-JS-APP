204d3807a3b59c52ef6039e982add35b003fc7c9
Wagura Maurice <wagura465@gmail.com>
2018-04-20 19:33:19 +0300

    integration with REST Countries API

--- app.js
+++ app.js
@@ -22,30 +22,63 @@ app.set('views', path.join(__dirname, 'views'));
 // body parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
 // express js middleware
 // app.use(express.static(path.join(__dirname, 'public')));
 
-var fs = require('fs');
-var file = __dirname + '/data.json';
+app.get('/', function(request, response) {
+	// get data from data.json file
+	var fs = require('fs');
+	var file = __dirname + '/data.json';
 
-fs.readFile(file, 'utf8', function (err, data) {
-  if (err) {
-    console.log('Error: ' + err);
-    return;
-  }
-
-  countries = JSON.parse(data);
-
-  // console.dir(data);
+	fs.readFile(file, 'utf8', function (error, data) {
+		if (error) {
+			console.log('Error: ' + error);
+			return;
+		}
+		response.render('region', {
+			title: "World Region",
+			data: JSON.parse(data)
+		});
+	});	
 });
 
-app.get('/', function(request, response) {
-	// response.send('Hello');
-	// response.json(countries);
-	response.render('home', {
-		title: "Countries API",
-		data: countries
-	});
+app.get('/region/:id', function(request, response) {
+
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4) {
+			response.render('region/countries', {
+				title: "Countries In " + request.params.id,
+				data: JSON.parse(this.responseText)
+			});
+		}
+	};
+
+	xhr.open("GET", "https://restcountries.eu/rest/v2/region/" + request.params.id);
+	xhr.send();
+
+});
+
+app.get('/country/:id', function(request, response) {
+
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var xhr = new XMLHttpRequest();
+
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4) {
+			response.render('country', {
+				title: request.params.id,
+				data: JSON.parse(this.responseText)
+			});
+		}
+	};
+
+	xhr.open("GET", "https://restcountries.eu/rest/v2/name/" + request.params.id + "?fullText=true");
+	xhr.send();
+
 });
 
 app.listen(port, function() {
